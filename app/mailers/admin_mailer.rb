ee2925ee365664eda24b0bb5fb3e7e8130f5e384
GamzeAkbal1 <gakbal112@gmail.com>
2016-07-29 10:54:39 +0300

    first commit

---
+++ app/mailers/admin_mailer.rb
@@ -0,0 +1,14 @@
+class AdminMailer < BaseMailer
+
+  def login_info(admin_id, password)
+    @admin      = Admin.find admin_id
+    @password   = password
+    @subject    = t('email.admin.login_info.title')
+    mail(to: @admin.email, subject: @subject)
+  end
+  def welcome admin_id
+    @admin      = Admin.find admin_id
+    @subject    = t('email.admin.login_info.title')
+    mail(to: @admin.email, subject: @subject)
+  end
+end
